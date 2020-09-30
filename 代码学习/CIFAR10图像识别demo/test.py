914c9de1b6a40f41190760b07de6c39ae24e3ffd
BrightHaozi <956540778@qq.com>
2020-09-30 09:50:12 +0800

    测试集测试

---
+++ 代码学习/CIFAR10图像识别demo/test.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# from ImageClassifier import Net
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1=nn.Conv2d(3,6,5)
+        self.pool=nn.MaxPool2d(2,2)
+        self.conv2=nn.Conv2d(6,16,5)
+        self.fc1=nn.Linear(16*5*5,120)
+        self.fc2=nn.Linear(120,84)
+        self.fc3=nn.Linear(84,10)
+
+    def forward(self,x):
+        x=self.pool(F.relu(self.conv1(x)))
+        x=self.pool(F.relu(self.conv2(x)))
+        x=x.view(-1,16*5*5) # 对应李宏毅教授课程中的flatten
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
+        x=self.fc3(x)
+        return x
+
+# Test the network on the test data
+dataiter=iter(testloader)
+# print("testloader:",testloader)
+# print("dataiter:",dataiter)
+images,labels=dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+PATH='./cifar_net.pth'
+net=Net()
+net.load_state_dict(torch.load(PATH))
+
+outputs=net(images)
+# print("outputs:",outputs,outputs.size())
+# torch.max(tensor,dim),每次输出两个tensor。当dim==0时，输出每列最大元素构成的tensor和最大元素所在位置构成的tensor
+# 当dim==1时，输出每行
+_ , predicted=torch.max(outputs,1)
+# print("predicted:",_,predicted)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images,labels=data
+        outputs=net(images)
+        _,predicted=torch.max(outputs.data,1)
+        total+=labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
